30618d8d90a0a75bc577714b1d0fa3db61c91bbe
van <one70201@gmail.com>
2022-04-20 21:48:04 +0800

    1

---
+++ 6.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+url = 'https://www.tnet.org.tw/Article/Detail/32773?type=%E7%94%A2%E6%A5%AD%E8%AD%B0%E9%A1%8C_%E7%94%A2%E6%A5%AD%E7%B6%9C%E8%A7%80&species=Slave&backPath=%2FArticle%2FMaster%2F8!%2FArticle%2FSlave%2F8%2F195'
+r = requests.get(url)
+sp = BeautifulSoup(r.text, 'lxml')
+a = sp.find('main', class_='article__main')
+a1 = a.find_all('p')
+for i in a1:
+   print(i.text)
+print()
