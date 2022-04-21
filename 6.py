f258c70ba7a4246ee77b5ce40fd5f303866be4b5
Evan <evan40907870@gmail.com>
2022-04-21 20:06:50 +0800

    1

--- 6.py
+++ 6.py
@@ -4,7 +4,7 @@ url = 'https://www.tnet.org.tw/Article/Detail/32773?type=%E7%94%A2%E6%A5%AD%E8%A
 r = requests.get(url)
 sp = BeautifulSoup(r.text, 'lxml')
 a = sp.find('main', class_='article__main')
-a1 = a.find_all('p')
-for i in a1:
-   print(i.text)
-print()
+a1 = a.find_all('h3')
+a2 = a.find_all('p')
+print(a1[2].text)
+print(a2[17].text)
