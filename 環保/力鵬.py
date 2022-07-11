5dc7f1d8035145e4ff2b8895c470bf0480798aae
Evan <evan40907870@gmail.com>
2022-07-11 19:50:00 +0800

    1

--- 環保/力鵬.py
+++ 環保/力鵬.py
@@ -5,5 +5,7 @@ url = 'https://www.lealeagroup.com.tw/libolon-detail/LIPENGE/'
 r = requests.get(url)
 sp = BeautifulSoup(r.text, 'lxml')
 sb = sp.find('div', class_='textEditor')
+for i in sb:
+   print(i.text , end="")
 
 
