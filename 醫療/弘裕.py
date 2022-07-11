6d883467db8427484c0fe80767304c94023a1f47
Evan <evan40907870@gmail.com>
2022-07-11 15:35:37 +0800

    1

--- 醫療/弘裕.py
+++ 醫療/弘裕.py
@@ -4,21 +4,7 @@ from bs4 import BeautifulSoup
 url = 'https://www.textile-hy.com.tw/Medical-Garment.htm#con_4945'
 r = requests.get(url)
 sp = BeautifulSoup(r.text, 'lxml')
-sb = sp.find('div', class_='lb-overlay').text
-sc = sp.find_all('h3')
-sa = sp.find_all('p')
-print(sb)
-for i in sc:
-    if i.text == '.':
-            print()
-    else:
-        print(i.text)
-print()
-
-print(sb)
-for i in sa:
-    if i.text == '.':
-            print()
-    else:
-        print(i.text)
-print()
+sb = sp.find_all('div', class_='lb-overlay')
+for i in sb:
+    print(i.h3.text)
+    print(i.p.text)
